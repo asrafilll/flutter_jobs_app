ca2a66157116d9f7aade60546b66bd154c5a8670
asrafilll <asrafilr@gmail.com>
2023-01-31 01:27:38 +0700

    Add Sign In Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:jobs_app/bmi_apps/height_provider.dart';
-import 'package:jobs_app/bmi_apps/main.dart';
-import 'package:jobs_app/bmi_apps/weight_provider.dart';
+// import 'package:jobs_app/bmi_apps/height_provider.dart';
+// import 'package:jobs_app/bmi_apps/main.dart';
+import 'package:jobs_app/main_apps/sign_in_page.dart';
+import 'package:jobs_app/main_apps/splash_screen.dart';
+// import 'package:jobs_app/bmi_apps/weight_provider.dart';
 // import 'package:jobs_app/gender_apps/gender_provider.dart';
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,23 +16,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider<WeightProvider>(
-          create: (context) => WeightProvider(),
-        ),
-        ChangeNotifierProvider<HeightProvider>(
-          create: (context) => HeightProvider(),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: const BMIApp(),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => const SplashScreen(),
+        '/sign-in': (context) => const SignInPage(),
+      },
     );
   }
 }
---
+++ lib/main_apps/splash_screen.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/sign-in');
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0XFF4141A4),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/logo.png',
+              width: 100,
+            ),
+            const SizedBox(height: 16),
+            const Text(
+              'FUTURE JOB',
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                fontSize: 32,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
